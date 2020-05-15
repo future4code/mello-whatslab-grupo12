6246b0a37e5a49a504af30a2821800d5994645a2
Ronaldo Jonson <ronaldojonson@hotmail.com>
2020-05-15 17:00:01 -0300

    CSS atualizado usando StyledComponents

--- projetogrupo12/src/App.js
+++ projetogrupo12/src/App.js
@@ -1,82 +1,127 @@
 import React from 'react';
-import logo from './logo.svg';
 
 import './App.css';
-import styled from 'styled-components'
+import styled from 'styled-components';
+
+const AppContent = styled.div`
+  background-color: white;
+  width: 50vw;
+  height: 100%;
+  margin: auto;
+  border-style: solid;
+  border-radius: 1mm;
+  border-color: black;
+`
+
+const AppHeader = styled.div`
+  background-color: #009688;
+  height: 10vh;
+`
+
+const Title = styled.h1`
+  color: white;
+  margin-left:10px;
+`
+
+const MessageFeed = styled.div`
+  background-color: white;
+  margin-left:10px;
+  padding-top: 25px;
+  padding-bottom: 50px;
+`
+
+const MessageStyle = styled.p`
+  padding-bottom: 5px;
+`
+
+const AppFooter = styled.div`
+  background-color: #f0f0f0;
+  height: 5vh;
+`
+const InputField = styled.div`
+  margin-left:10px;
+  height: 100%;
+  display: flex;
+  align-items: center;
+`
 
 class App extends React.Component {
     state = {
     
-    pessoas: [
+      mensagems: [
       {
-        usuario: "",
-        mensagem: "",
-        
+        usuario: "Labezap",
+        texto: "Olá, essa é a primeira mensagem",        
       },
       
     ],
     
     valorInputUsuario: "",
-    valorInputMensagem: "",
+    valorInputTexto: "",
     
   };
 
-  adicionaPessoa = () => {
+  adicionarMensagem = () => {
     
-    const novaPessoa = {
+    const novaMensagem = {
      
       usuario: this.state.valorInputUsuario,
-      mensagem: this.state.valorInputMensagem,
+      texto: this.state.valorInputTexto,
       };
 
-     const novoPessoas = [...this.state.pessoas, novaPessoa];
-
-    this.setState({ pessoas: novoPessoas });
+      if(novaMensagem.texto != "" && novaMensagem.usuario != "")
+      {
+          const novasMensagems = [...this.state.mensagems, novaMensagem];
+          this.setState({ valorInputTexto: "" });
+        
+          this.setState({ mensagems: novasMensagems });
+      }
   };
 
   onChangeInputUsuario = event => {
    this.setState({ valorInputUsuario: event.target.value });
   };
 
-  onChangeInputMensagem = event => {
-    this.setState({ valorInputMensagem: event.target.value });
+  onChangeInputTexto = event => {
+    this.setState({ valorInputTexto: event.target.value });
   };
 
   
   render() {
    
-    const listaDeComponentes = this.state.pessoas.map(pessoa => {
+    const listaDeComponentes = this.state.mensagems.map(pessoa => {
       return (
-        <p>
-          {pessoa.usuario} - {pessoa.mensagem} 
-        </p>
+        <MessageStyle>
+          <b>{pessoa.usuario}</b> : {pessoa.texto} 
+        </MessageStyle>
       );
     });
 
     return (
-      <div className={'App-header'}>
-        <h1>Lista de Mensagem</h1>
-        <div>
-          <input
-           
-            value={this.state.valorInputUsuario}
-            
-            onChange={this.onChangeInputUsuario}
-            placeholder={"Usuário"}
-          />
-          <input
-            
-            value={this.state.valorInputMensagem}
-            
-            onChange={this.onChangeInputMensagem}
-            placeholder={"Mensagem"}
-          />
-
-          
-          <button onClick={this.adicionaPessoa}>Enviar</button>
-        </div>
-        <div>{listaDeComponentes}</div>
-      </div>
+      <AppContent>
+        <AppHeader>
+          <Title>Lista de Mensagem</Title>
+        </AppHeader>
+        <MessageFeed>{listaDeComponentes}</MessageFeed>
+        <AppFooter>
+          <InputField>
+            <input           
+              value={this.state.valorInputUsuario}            
+              onChange={this.onChangeInputUsuario}
+              placeholder={"Usuário"}
+              size="15"
+            />
+            <input
+              type="text"
+              value={this.state.valorInputTexto}           
+              onChange={this.onChangeInputTexto}
+              placeholder={"Mensagem"}
+              size="50"
+            />
+            <button onClick={this.adicionarMensagem}>Enviar</button>
+          </InputField>
+        </AppFooter>
+      </AppContent>
     );
   }
 }
